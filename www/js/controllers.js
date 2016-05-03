79daffd3db94205000fbd1178be2b45e9a43da4a
Swadeepreddy <Swadeepreddy@outlook.com>
2016-05-03 14:39:06 +0530

    With basic navigation as mejestic busstand as destination

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
-angular.module('myApp.controllers', ['ionic'])
+var gpsapp = angular.module('myApp.controllers', ['ionic']);
 
 
-.controller('NavCtrl', function($scope, $ionicLoading, $compile) {
+gpsapp.controller('LoginHomeCtrl', function($scope, $ionicLoading, $compile) {
   var myLatLng;
   function initialize() {
 
@@ -31,6 +31,51 @@ angular.module('myApp.controllers', ['ionic'])
   ionic.Platform.ready(initialize)
 });
 
+gpsapp.controller('NavCtrl', function($scope, $ionicLoading, $compile) {
+  var myLatLng, targetLatLng;
+  var map;
+  function initializeRoute() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      myLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);targetLatLng = new google.maps.LatLng(12.978020, 77.572327);
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      console.log(myLatLng);
+      var mapOptions =
+              {
+                  zoom: 9,
+                  center: myLatLng,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP,
+              };
+
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      $scope.map = map;
+      directionsDisplay.setMap(map);
+      calcRoute(myLatLng, targetLatLng);
+    });
+    
+    /*
+    navigator.geolocation.getCurrentPosition(function(position) {
+    
+        var mapOptions = {
+          center: myLatLng,
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+        
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Your Pos'
+        });
+
+    });
+    */
+    
+  } 
+
+  ionic.Platform.ready(initializeRoute)
+});
 /*
 // http://stackoverflow.com/questions/29339145/implement-google-map-directions-using-angular-google-map-in-ionic
 
@@ -48,19 +93,6 @@ angular.module('myApp.controllers', ['ionic'])
 
 
 var exampleApp=angular.module('starter', ['ionic'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-});
 
 var directionsDisplay;
     var directionsService = new google.maps.DirectionsService();
@@ -83,9 +115,13 @@ var directionsDisplay;
     };
 google.maps.event.addDomListener(window, 'load', initialize); 
 
-function calcRoute() {
-  var start = "your start latlng here";
-  var end = "your destinationl latlng here";
+
+*/
+
+
+
+function calcRoute(start, end) {
+  var directionsService = new google.maps.DirectionsService();
   var request = {
       origin:start,
       destination:end,
@@ -97,4 +133,3 @@ function calcRoute() {
     }
   });
 }
-*/
